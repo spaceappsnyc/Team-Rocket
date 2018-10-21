b85babbf1a2f8cf0af5777cb93752c3fbc593ef7
Joseph Pacura <jpacura96@gmail.com>
2018-10-21 05:32:13 -0400

    Added API for Year to date launches and next launch

--- routes/rockets.js
+++ routes/rockets.js
@@ -9,5 +9,20 @@ router.get('/', function(req, res, next) {
         .catch(console.warn);
 });
 
+router.get('/stats', function(req, res, next) {
+    const getYtdCountPromise = getYtdCount();
+    const getNextLaunchPromise = getNextLaunch();
+    Promise.all([getYtdCountPromise, getNextLaunchPromise]).then(json => res.json(json));
+});
+
+function getNextLaunch() {
+    return axios.get('https://launchlibrary.net/1.4/launch/next/1')
+        .then(data => ({ nextlaunch: data.data.launches[0].windowstart, nextcountry: data.data.launches[0].location.countryCode }));
+}
+
+function getYtdCount() {
+    return axios.get('https://launchlibrary.net/1.4/launch/2018-01-01/2018-10-21') // TODO: MAKE THIS DYNAMIC
+        .then(data => ({ getYtdCount: data.data.total }));
+}
 
 module.exports = router;
