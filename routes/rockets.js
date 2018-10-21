5272293f818b33d59813f03237052cc6576cfe36
Joseph Pacura <jpacura96@gmail.com>
2018-10-21 05:50:59 -0400

    Added next 20 launches API endpoint

--- routes/rockets.js
+++ routes/rockets.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    axios.get('https://launchlibrary.net/1.4/rocket/1')
+router.get('/next', function(req, res, next) {
+    axios.get('https://launchlibrary.net/1.4/rocket/next/20')
         .then(data => res.json(data.data))
         .catch(console.warn);
 });
